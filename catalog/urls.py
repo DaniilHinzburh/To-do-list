264e698b676711a7e4f187405fc420781cce0424
ilgan <2ilgan1601@gmail.com>
2024-02-20 18:30:54 +0300

    task completed(4)

--- catalog/urls.py
+++ catalog/urls.py
@@ -22,12 +22,12 @@ urlpatterns = [
         name="task_update",
     ),
     path(
-        "task/<int:pk>/delete",
+        "task/<int:pk>/delete/",
         TaskDeleteView.as_view(),
         name="task_delete",
     ),
     path(
-        "task/<int:pk>/complete",
+        "task/<int:pk>/complete/",
         CompleteTaskView.as_view(),
         name="task_complete",
     ),
@@ -48,7 +48,7 @@ urlpatterns = [
         name="tag_update",
     ),
     path(
-        "tag/<int:pk>/delete",
+        "tag/<int:pk>/delete/",
         TagDeleteView.as_view(),
         name="tag_delete",
     ),
